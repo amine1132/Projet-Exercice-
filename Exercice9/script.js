31d866099e20aa37c148be9919b8313cb4bfb5f4
Amine Sekkiou <amine1132@hotmail.fr>
2025-02-14 13:57:48 +0100

    correction de l'exercice 9

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -2,11 +2,11 @@
 
 function checkAge(age) {
   if (age < 18) {
-    console.log("Vous êtes mineur.");
+    return "Vous êtes mineur.";
   } else if (age <= 65) {
-    console.log("Vous êtes majeur.");
+    return "Vous êtes majeur.";
   } else {
-    console.log("Vous êtes senior.");
+    return "Vous êtes senior.";
   }
 }
 
